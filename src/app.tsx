8281f559e5eaa61d3e92534ed7350ae81e0e7e6c
Peter-WebDev <peter.warholm@gmail.com>
2025-12-08 20:50:10 +0100

    feat: add meta data support with solid-meta

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import { ColorModeProvider, ColorModeScript, cookieStorageManagerSSR } from "@kobalte/core";
+import { Link, Meta, MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
@@ -18,10 +19,18 @@ export default function App() {
     <Router
       root={props => (
         <>
-          <ColorModeScript storageType={storageManager.type} />
-          <ColorModeProvider storageManager={storageManager}>
-            <Suspense>{props.children}</Suspense>
-          </ColorModeProvider>
+          <MetaProvider>
+            <Title>Dshbloks</Title>
+            <Meta name="description" content="Dshbloks - Information in a dash" />
+            <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <Link rel="manifest" href="/site.webmanifest" />
+            <ColorModeScript storageType={storageManager.type} />
+            <ColorModeProvider storageManager={storageManager}>
+              <Suspense>{props.children}</Suspense>
+            </ColorModeProvider>
+          </MetaProvider>
         </>
       )}
     >
