a743572ef46bd33b0ebd265c5ba2eb5cae28b219
Peter <peter.warholm@gmail.com>
2025-12-28 11:40:40 +0100

    fix: refactor authentication handler to improve request logging and OPTIONS response

--- src/routes/api/auth/[...all].ts
+++ src/routes/api/auth/[...all].ts
@@ -1,4 +1,25 @@
 import { toSolidStartHandler } from 'better-auth/solid-start';
 import { auth } from '~/lib/auth';
 
-export const { GET, POST } = toSolidStartHandler(auth);
+const handler = toSolidStartHandler(auth);
+
+export const GET = async (event: any) => {
+  console.log('[auth] GET', event?.request?.url);
+  return handler.GET?.(event);
+};
+
+export const POST = async (event: any) => {
+  console.log('[auth] POST', event?.request?.url);
+  return handler.POST?.(event);
+};
+
+// Hantera preflight / OPTIONS så den inte returnerar 404
+export const OPTIONS = async () =>
+  new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
