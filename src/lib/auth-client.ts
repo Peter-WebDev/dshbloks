9cdd10d3de91e1f6fe8732f6e371c9afc74a6603
Peter <peter.warholm@gmail.com>
2025-12-28 11:14:00 +0100

    fix: adjust authentication client setup with environment-based baseURL

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,8 @@
-import { createAuthClient } from "better-auth/solid"
-export const { signIn, signOut, signUp, changePassword, useSession } = createAuthClient({
-
-})
+import { createAuthClient } from 'better-auth/solid';
+export const { signIn, signOut, signUp, changePassword, useSession } =
+  createAuthClient({
+    baseURL:
+      process.env.NODE_ENV === 'production'
+        ? 'https://dshbloks.popjosef.se'
+        : 'http://localhost:3000',
+  });
